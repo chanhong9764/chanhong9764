e6dc8425c1a943ba3aa6f27f3ba73f70f5e0eeb7
Chanhong Park <chanhong9784@naver.com>
2024-02-19 22:26:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -104,6 +104,8 @@ markdown_text = """
 |---|---|
 |[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=chanhong9764&langs_count=10&layout=compact&theme=dark)](https://github.com/chanhong9764/chanhong9764)
 """ # list of blog posts will be appended here
+markdown_text.lstrip()
+markdown_text.rstrip()
 markdown_text += "|"
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
