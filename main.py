373f15cea2576fc013a1a06af79fe409fc8af57f
Chanhong Park <chanhong9784@naver.com>
2024-02-19 22:06:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -112,8 +112,8 @@ for idx, feed in enumerate(RSS_FEED['entries']):
     else:
         feed_date = feed['published_parsed']
         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-markdown_text = "|"
-markdown_text = "</div>"
+markdown_text += "|"
+markdown_text += "</div>"
 f = open("README.md",mode="w", encoding="utf-8")
 f.write(markdown_text)
 f.close()
